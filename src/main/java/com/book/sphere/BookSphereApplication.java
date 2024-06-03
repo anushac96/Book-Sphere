ec87a6dec1786102d79002e510f45ef5925237ab
anushac96 <anushac96@gmail.com>
2024-06-02 17:25:28 -0700

    project setup

---
+++ src/main/java/com/book/sphere/BookSphereApplication.java
@@ -0,0 +1,13 @@
+package com.book.sphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSphereApplication.class, args);
+	}
+
+}
