14fc6d6e0cdbd857aa5970bab08c4d8817dd4c5b
anushac96 <anushac96@gmail.com>
2024-07-19 16:06:45 -0700

    added role repo

--- src/main/java/com/book/sphere/role/RoleRepository.java
+++ src/main/java/com/book/sphere/role/RoleRepository.java
@@ -1,5 +1,9 @@
 package com.book.sphere.role;
 
-public interface RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository< Role, Integer>{
+	Optional<Role> findByName(String role);
 }
