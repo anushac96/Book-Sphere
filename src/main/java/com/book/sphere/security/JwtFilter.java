1408d92a76746d8e12ef5cb9d45b1886db980c43
anushac96 <anushac96@gmail.com>
2024-07-20 00:08:03 -0700

    Implemented the JwtFilter class and JwtService class

--- src/main/java/com/book/sphere/security/JwtFilter.java
+++ src/main/java/com/book/sphere/security/JwtFilter.java
@@ -2,22 +2,61 @@ package com.book.sphere.security;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Id;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
+import org.springframework.http.HttpHeaders;
+
+//@Component
+//public class JwtFilter extends GenericFilterBean {
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        // Implement JWT validation and authentication logic here
+//        chain.doFilter(request, response);
+//    }
+//}
+
 
 @Component
-public class JwtFilter extends GenericFilterBean {
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter{
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        // Implement JWT validation and authentication logic here
-        chain.doFilter(request, response);
-    }
+	private final JwtService jwtService;
+	
+	@Override
+	protected void doFilterInternal(
+			@NotNull HttpServletRequest request, 
+			@NotNull HttpServletResponse response, 
+			@NotNull FilterChain filterChain)
+			throws ServletException, IOException {
+		if(request.getServletPath().contains("/api/v1/auth")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+		final String jwt;
+        final String userEmail;
+        
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+	}
+	
 }
