e42c8d1f86297927c248cfe1cd6ba3f05a195114
anushac96 <anushac96@gmail.com>
2024-07-19 21:03:07 -0700

    Created the Token entity and repository

---
+++ src/main/java/com/book/sphere/user/Token.java
@@ -0,0 +1,40 @@
+package com.book.sphere.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+	
+	@Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+	
+}
